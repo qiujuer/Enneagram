bff9a011d26625c7e6732684e06c3f39606608e5
Qiujuer <qiujuer@live.cn>
2015-09-19 12:13:00 +0800

    Submit need files

---
+++ js/data.js
@@ -0,0 +1,22 @@
+[
+    [
+        {
+            "name": "t1-name1",
+            "value": "e"
+        },
+        {
+            "name": "t1-name2",
+            "value": "b"
+        }
+    ],
+    [
+        {
+            "name": "t2-name1",
+            "value": "e"
+        },
+        {
+            "name": "t2-name2",
+            "value": "b"
+        }
+    ]
+]
